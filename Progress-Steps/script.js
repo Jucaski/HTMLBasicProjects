0131b20313b228ac33757600961714194c6d84dd
Karla <kjuradoj@gmail.com>
2023-08-04 16:00:15 -0600

    Adding variables in the script for progress steps

---
+++ Progress-Steps/script.js
@@ -0,0 +1,7 @@
+const progressBar = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+
+let currentCircleActive = 1
+
