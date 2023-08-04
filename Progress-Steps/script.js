1e43804a5949041803a43045f2f7bb3731c6c801
Karla <kjuradoj@gmail.com>
2023-08-04 16:08:54 -0600

    Giving color to the active circles

--- Progress-Steps/script.js
+++ Progress-Steps/script.js
@@ -5,3 +5,32 @@ const circles = document.querySelectorAll('.circle')
 
 let currentCircleActive = 1
 
+//Adding functionality to the next button
+next.addEventListener('click', ()=>{
+  currentCircleActive++;
+
+  if(currentCircleActive > circles.length){
+    currentCircleActive = circles.length
+  }
+  update()
+})
+
+//Adding funcionality to the previous button
+prev.addEventListener('click', ()=>{
+  currentCircleActive--;
+  if(currentCircleActive < 1){
+    currentCircleActive = 1;
+  }
+  update()
+})
+
+//We will color the progress bar and the number circles
+function update(){
+  circles.forEach((circle, index) => {
+    if(index < currentCircleActive) {
+      circle.classList.add('active')
+    } else {
+      circle.classList.remove('active')
+    }
+  })
+}
